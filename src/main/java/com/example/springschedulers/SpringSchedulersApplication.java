15dbfd2c91ce825ab31820cbb549c0bf8ad701d5
Gustavo Fernandez <gustavocfernandez@bcp.com.pe>
2022-01-13 19:42:30 -0500

    Project setup

---
+++ src/main/java/com/example/springschedulers/SpringSchedulersApplication.java
@@ -0,0 +1,13 @@
+package com.example.springschedulers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSchedulersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSchedulersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springschedulers/SpringSchedulersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springschedulers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSchedulersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
